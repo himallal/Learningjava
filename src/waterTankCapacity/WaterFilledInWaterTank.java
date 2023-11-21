da3e1963a0f6af813f10ebd23ef49cc3595f09c0
himallal <himallal370@gmail.com>
2023-11-21 00:18:20 -0500

    assignment modified

--- src/waterTankCapacity/WaterFilledInWaterTank.java
+++ src/waterTankCapacity/WaterFilledInWaterTank.java
@@ -5,25 +5,33 @@ import java.util.Scanner;
 public class WaterFilledInWaterTank {
 	int tankCapacity = 100;
 	int bucketCapacity = 10;
+	int waterInTank;
+	int currentLevel;
 	int breakpoint=90;
-	
+	Scanner sc=new Scanner(System.in);
 	
 
 	void calculateTheWaterLevel() {
+		System.out.println("the current level of water");
+		currentLevel=sc.nextInt();
 
-		for (int waterIntank = 0; waterIntank <=breakpoint; waterIntank += bucketCapacity) {
-			
 		
-			System.out.println("the total amount of water in the tank" + waterIntank);
+	
+		while(currentLevel<=breakpoint) {
+		currentLevel+=bucketCapacity;
+		System.out.println("the watel level is"+currentLevel);
 		
-			 
-		if (waterIntank==breakpoint) {
+		 
+	if (currentLevel==breakpoint) {
 			
-			System.out.println("watertank will be overflow,the current water in tank"+waterIntank);
-				break;
+			System.out.println("watertank will be overflow,the current water in tank"+currentLevel);
+		
+			break;
+			
 			}
 			
+			}
 		}
 
 	}
-}
+
